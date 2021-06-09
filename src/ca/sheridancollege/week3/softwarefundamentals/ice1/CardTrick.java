18ffbed476694601d14fca82358d78c610c477ce
Aryan <Aryan@ADMINRG-OR1RP7Q>
2021-06-10 01:40:36 +0530

    added random function

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,7 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -16,14 +17,19 @@ import java.util.Scanner;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        int valuex = 0;
+        String suitx = null;
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            for (int i = 0; i < magicHand.length; i++) {
+        Scanner inputScan = new Scanner(System.in);
+        Random rand = new Random();
+
+        // Lucky Card logic
+//        Card luckyCard = new Card();
+//        luckyCard.setValue(7);
+//        luckyCard.setSuit("Hearts");
+        for (int i = 0; i < magicHand.length; i++) {
 //        for (Card magicHand1 : magicHand) {
             Card c = new Card();
             int r;
@@ -36,45 +42,5 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             magicHand[i] = c;
         }
-           boolean contLoop;
-
-        Card cUser = new Card();
-        do {
-            contLoop = false;
-            System.out.print("\nChoose a SUIT (1)Herats / (2)Diamonds / (3)Spades / (4)Clubs):\n");
-            try {
-                valuex = inputScan.nextInt();
-                switch (valuex) {
-                    case 1:
-                        suitx = "Hearts";
-                        break;
-                    case 2:
-                        suitx = "Diamonds";
-                        break;
-                    case 3:
-                        suitx = "Spades";
-                        break;
-                    case 4:
-                        suitx = "Clubs";
-                        break;
-                    default:
-                        throw new Exception();
-                }
-            } catch (Exception e) {
-                System.out.println("Only 1-4. Try Again.");
-                contLoop = true;
-            }
-            cUser.setSuit(suitx);
-            inputScan = new Scanner(System.in);
-        } while (contLoop);
 
         
-        }
-        
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
-}
