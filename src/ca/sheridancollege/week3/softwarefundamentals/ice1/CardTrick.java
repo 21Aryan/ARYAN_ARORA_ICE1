6a8faad5637f020f30bef409c33799f6c315e12a
Aryan <Aryan@ADMINRG-OR1RP7Q>
2021-06-10 01:27:46 +0530

    added scanner

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -34,8 +36,41 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             magicHand[i] = c;
         }
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+           boolean contLoop;
+
+        Card cUser = new Card();
+        do {
+            contLoop = false;
+            System.out.print("\nChoose a SUIT (1)Herats / (2)Diamonds / (3)Spades / (4)Clubs):\n");
+            try {
+                valuex = inputScan.nextInt();
+                switch (valuex) {
+                    case 1:
+                        suitx = "Hearts";
+                        break;
+                    case 2:
+                        suitx = "Diamonds";
+                        break;
+                    case 3:
+                        suitx = "Spades";
+                        break;
+                    case 4:
+                        suitx = "Clubs";
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            } catch (Exception e) {
+                System.out.println("Only 1-4. Try Again.");
+                contLoop = true;
+            }
+            cUser.setSuit(suitx);
+            inputScan = new Scanner(System.in);
+        } while (contLoop);
+
+        
         }
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
